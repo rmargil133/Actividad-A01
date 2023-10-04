909c2c541cdc019080d505df6b83d9207cec237a
Ruben Mariscal <92536400+rmargil133@users.noreply.github.com>
2023-10-04 20:53:28 +0200

    Add files via upload

---
+++ Ejercicio1/script.js
@@ -0,0 +1,5 @@
+//Muestra por consola el mensaje hola mundo
+console.log("Hola Mundo");
+
+//Muestra una alerta en pantalla con el mensaje Soy el porimer script.
+console.log("Soy el primer script")
